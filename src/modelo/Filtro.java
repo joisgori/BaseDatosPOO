001bb774d414a335dced2b7c312d313944376ba3
LN710Q <LN710Q@DEILABO1-PC13>
2018-06-14 15:32:58 -0600

    Cree mi bases de datos [LABOO POO]

---
+++ src/modelo/Filtro.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Filtro {
+    private int id;
+    private String codigo;
+    private String marca;
+    private int stock;
+    private boolean existencia;
+
+    public Filtro() {
+    }
+
+    public Filtro(int id, String codigo, String marca, int stock, boolean existencia) {
+        this.id = id;
+        this.codigo = codigo;
+        this.marca = marca;
+        this.stock = stock;
+        this.existencia = existencia;
+    }
+    
+    public Filtro(String codigo, String marca, int stock, boolean existencia) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.stock = stock;
+        this.existencia = existencia;
+    }
+    
+    public Filtro(String marca, int stock, boolean existencia) {
+        this.marca = marca;
+        this.stock = stock;
+        this.existencia = existencia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public boolean getExistencia() {
+        return existencia;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setExistencia(boolean existencia) {
+        this.existencia = existencia;
+    }
+    
+    
+    
+}
